286d1632e42688a413f70c9beb9fb94c7f2ffb32
flask <notflask@proton.me>
2024-09-29 09:01:23 +0200

    add neovim config

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,32 @@
+return {
+  { "NLKNguyen/papercolor-theme" },
+  {
+    "navarasu/onedark.nvim",
+    config = function()
+      require("onedark").setup({
+        style = "darker",
+      })
+    end,
+  },
+  { "morhetz/gruvbox" },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      window = { width = 27 },
+    },
+  },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "nvim-lualine/lualine.nvim", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "folke/which-key.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+}
